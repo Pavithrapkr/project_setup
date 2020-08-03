ae65fc7b0cbcaa197357249c4d1ac44603b18bdc
Pavithrapkr <pavithrakowsika@gmail.com>
2020-08-03 19:45:34 +0530

    testing cloud front deployment

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing Deployment
+          Testing cloudfront !!!
         </p>
         <a
           className="App-link"
